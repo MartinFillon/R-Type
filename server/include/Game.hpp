815509b9ad5ae169ded40db4f89e2101ead79842
Paul Bardeur <paul.bardeur@epitech.eu>
2024-09-24 17:15:26 +0200

    feat(server): add a class game to handle game process

---
+++ server/include/Game.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-PAR-5-1-rtype-edward.lalande
+** File description:
+** Game
+*/
+
+#ifndef GAME_HPP_
+    #define GAME_HPP_
+
+namespace Rtype {
+
+    class Game {
+
+        public:
+
+            Game() = default;
+
+            void update() {};
+            void processAction() {};
+
+    };
+
+};
+
+#endif /* !GAME_HPP_ */
