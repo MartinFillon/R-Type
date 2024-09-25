e65aede19888bffda9f03c30a941ace05293f315
codEWard-edward <edward.lalande@epitech.eu>
2024-09-25 19:42:50 +0200

    feat(Components): Life component for entity that have life

---
+++ ecs/include/Components/Life.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Life
+*/
+
+#ifndef LIFE_HPP_
+    #define LIFE_HPP_
+
+namespace ecs {
+    namespace component {
+        struct Life {
+            double _life;
+        };
+    };
+};
+
+#endif /* !LIFE_HPP_ */
