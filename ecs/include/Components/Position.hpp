55b8c25b55c5c066b043e1a07e97835a64a2e22e
codEWard-edward <edward.lalande@epitech.eu>
2024-09-25 19:17:15 +0200

    feat(Components): Position component for entity to get a position

---
+++ ecs/include/Components/Position.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Position
+*/
+
+#ifndef POSITION_HPP_
+    #define POSITION_HPP_
+
+namespace ecs {
+    namespace component {
+        struct Position {
+            double _x;
+            double _y;
+        };
+    };
+};
+
+#endif /* !POSITION_HPP_ */
