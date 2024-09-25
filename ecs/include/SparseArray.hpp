87952ee4bbef8bdb8cdb7e20780bd94bb022f837
codEWard-edward <edward.lalande@epitech.eu>
2024-09-25 16:42:20 +0200

    feat(SparseArray): an array of templated component where we can act on it

---
+++ ecs/include/SparseArray.hpp
@@ -0,0 +1,50 @@
+#ifndef SPARSEARRAY_HPP_
+#define SPARSEARRAY_HPP_
+
+#include <vector>
+#include <optional>
+
+namespace ecs {
+    template <typename Component>
+    class SparseArray {
+        public:
+            using value_type = std::optional<Component>;
+            using reference_type = value_type &;
+            using const_reference_type = value_type const &;
+            using container = std::vector<value_type>;
+
+            SparseArray() {}
+            SparseArray(SparseArray const &other) : _data(other._data) {}
+            SparseArray(SparseArray &&other) noexcept : _data(std::move(other._data)) {}
+
+            SparseArray &operator=(SparseArray const &other) {
+                if (this != &other) {
+                    _data = other._data;
+                }
+                return *this;
+            }
+
+            SparseArray &operator=(SparseArray &&other) noexcept {
+                if (this != &other) {
+                    _data = std::move(other._data);
+                }
+                return *this;
+            }
+
+            reference_type operator[](size_t idx) {
+                if (idx >= _data.size()) {
+                    _data.resize(idx + 1);
+                }
+                return _data[idx];
+            }
+
+            std::size_t size() const {
+                return _data.size();
+            }
+
+        private:
+            container _data;
+    };
+}
+
+#endif /* !SPARSEARRAY_HPP_ */
