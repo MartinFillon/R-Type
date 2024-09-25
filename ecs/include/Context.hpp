302d1c2e600efb4f7896f3d2508bfaa08be2fff8
MartinFillon <martin.fillon@hotmail.com>
2024-09-25 10:23:14 +0200

    feat(build): create build files for subdir lib ecs

---
+++ ecs/include/Context.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** rtype
+** File description:
+** Context
+*/
+
+#pragma once
+
+class Context {
+  public:
+    Context();
+    ~Context();
+
+  protected:
+  private:
+};
