82b397c86e2e05874ab6774ad59265fb8b696812
Paul Bardeur <paul.bardeur@epitech.eu>
2024-09-25 13:22:10 +0200

    feat(server): add Packet class to create and send packet to client

---
+++ server/include/Packet.hpp
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-PAR-5-1-rtype-edward.lalande
+** File description:
+** Packet
+*/
+
+#ifndef PACKET_HPP_
+    #define PACKET_HPP_
+
+    #include <vector>
+    #include <memory>
+
+    #define MESSAGE_OPCODE 4
+    #define MESSAGE_MIN_SIZE 5
+    #define GET_MAGIC_NUMBER(x) (x[0] << 24) | (x[1] << 16) | (x[2] << 8) | x[3]
+
+namespace Rtype {
+
+    class Packet {
+
+        using Message = std::vector<uint8_t>;
+        using Arguments = std::vector<uint8_t>;
+
+        public:
+
+            Packet(const Message &message);
+            Packet(uint8_t opcode, Arguments arguments = {});
+
+            bool isValid() const;
+            uint8_t getOpcode() const;
+            Arguments getArguments() const;
+
+            Message toMessage() const;
+
+        private:
+
+            bool isValidOpcode(uint8_t opcode);
+
+            bool _isValid;
+
+            uint32_t _magicNumber;
+            uint8_t _opcode;
+            Arguments _arguments;
+
+
+    };
+
+};
+
+#endif /* !PACKET_HPP_ */
---
+++ server/src/Packet.cpp
@@ -0,0 +1,86 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-PAR-5-1-rtype-edward.lalande
+** File description:
+** Packet
+*/
+
+#include "Packet.hpp"
+
+Rtype::Packet::Packet(const Message &message): _isValid(false)
+{
+    if (message.size() < MESSAGE_MIN_SIZE) {
+        return;
+    }
+
+    _magicNumber = GET_MAGIC_NUMBER(message);
+
+    const uint32_t BEER = 0x42454552;
+
+    if (_magicNumber != BEER) {
+        return;
+    }
+
+    _opcode = message[MESSAGE_OPCODE];
+
+    if (!isValidOpcode(_opcode)) {
+        return;
+    }
+
+    _arguments.assign(message.begin() + MESSAGE_OPCODE + 1, message.end());
+
+    _isValid = true;
+}
+
+Rtype::Packet::Packet(uint8_t opcode, Arguments arguments) : _isValid(false)
+{
+    _magicNumber = 0x42454552;
+
+    if (!isValidOpcode(opcode)) {
+        return;
+    }
+
+    _opcode = opcode;
+    _arguments = arguments;
+
+    _isValid = true;
+}
+
+bool Rtype::Packet::isValid() const
+{
+    return _isValid;
+}
+
+uint8_t Rtype::Packet::getOpcode() const
+{
+    return _opcode;
+}
+
+Rtype::Packet::Arguments Rtype::Packet::getArguments() const
+{
+    return _arguments;
+}
+
+Rtype::Packet::Message Rtype::Packet::toMessage() const
+{
+    Message message;
+
+    message.push_back(static_cast<uint8_t>((_magicNumber >> 24) & 0xFF));
+    message.push_back(static_cast<uint8_t>((_magicNumber >> 16) & 0xFF));
+    message.push_back(static_cast<uint8_t>((_magicNumber >> 8) & 0xFF));
+    message.push_back(static_cast<uint8_t>(_magicNumber & 0xFF));
+
+    message.push_back(_opcode);
+
+    message.insert(message.end(), _arguments.begin(), _arguments.end());
+
+    return message;
+}
+
+bool Rtype::Packet::isValidOpcode(uint8_t opcode)
+{
+    // std::vector<uint8_t> validOpcodes = { 0x01, 0x02, ... };
+    // return std::find(validOpcodes.begin(), validOpcodes.end(), opcode) != validOpcodes.end();
+
+    return true;
+}
