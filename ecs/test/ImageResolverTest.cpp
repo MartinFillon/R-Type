c07997f13325405b5ae7b76c59964d97186ffaf9
MartinFillon <martin.fillon@hotmail.com>
2024-09-25 15:23:34 +0200

    test: add testing of the cache for the image loader

--- ecs/test/ImageResolverTest.cpp
+++ ecs/test/ImageResolverTest.cpp
@@ -5,14 +5,23 @@
 ** ImageResolverTest
 */
 
-#include "ImageResolver.hpp"
-
 #include <criterion/assert.h>
 #include <criterion/criterion.h>
+
+#define private public
+#include "ImageResolver.hpp"
 
 Test(ImageResolver, test1)
 {
     ECS::ImageResolver resolver("./");
     std::string file = resolver.getImage("README.md");
     cr_assert_strings_neq(file.c_str(), "");
+}
+
+Test(ImageResolver, test2)
+{
+    ECS::ImageResolver resolver("./");
+    std::string file = resolver.getImage("README.md");
+
+    cr_assert_strings_eq(resolver._cache.at("README.md").c_str(), file.c_str());
 }
