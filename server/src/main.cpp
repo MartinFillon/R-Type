3f3b86c61a2566654a7e6363f1f4e651d50403d7
MartinFillon <martin.fillon@hotmail.com>
2024-09-25 15:53:09 +0200

    chore(server): restore main

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -5,34 +5,27 @@
 ** main
 */
 
-#include <iostream>
-
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
 
-#include "ImageResolver.hpp"
+#include "Context.hpp"
 
 int main(void)
 {
-    // sf::RenderWindow window(sf::VideoMode(800, 600), "ECS SFML Example");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "ECS SFML Example");
 
-    // ECS::Context ctx = ECS::Context(window);
+    ECS::Context ctx = ECS::Context(window);
 
-    // while (window.isOpen()) {
-    //     sf::Event event;
-    //     while (window.pollEvent(event)) {
-    //         if (event.type == sf::Event::Closed)
-    //             window.close();
-    //     }
-    //     window.clear();
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
 
-    //     window.display();
-    // }
-
-    ECS::ImageResolver resolver("./");
-
-    std::cout << resolver.getImage("README.md") << std::endl;
-    std::cout << resolver.getImage("README.md") << std::endl;
+        window.display();
+    }
     return 0;
 }
