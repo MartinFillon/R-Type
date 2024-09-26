00bf167b2246e97b89102880e059bbd468b015ee
codEWard-edward <edward.lalande@epitech.eu>
2024-09-26 12:25:29 +0200

    fix(Components): SoundEffects, changes from sf::Music to sf::Sound

--- ecs/include/Components/SoundEffect.hpp
+++ ecs/include/Components/SoundEffect.hpp
@@ -8,13 +8,13 @@
 #ifndef SOUNDEFFECT_HPP_
     #define SOUNDEFFECT_HPP_
 
-    #include <SFML/Audio/Music.hpp>
+    #include <SFML/Audio/Sound.hpp>
     #include <string>
 
 namespace ecs {
     namespace component {
         struct SoundEffect {
-            sf::Music _sound;
+            sf::Sound _sound;
             std::string _pathToSound;
         };
     };
