fb54d8f419d0a8530b18b1887b2214a960871572
codEWard-edward <edward.lalande@epitech.eu>
2024-09-26 10:27:58 +0200

    feat(Components): Outlined color components for object outlined with a color like shape, text...

---
+++ ecs/include/Components/OutlinedColorComponent.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** OutlinedColor
+*/
+
+#ifndef OUTLINEDCOLOR_HPP_
+    #define OUTLINEDCOLOR_HPP_
+
+#include <SFML/Graphics/Color.hpp>
+
+namespace ecs {
+    namespace component {
+        struct OutlinedColor {
+            sf::Color _outlinedColor;
+        };
+    };
+};
+
+#endif /* !OUTLINEDCOLOR_HPP_ */
