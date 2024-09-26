50dd8861b37c6ac0237c18cc7633cfadf3763e27
Ravriely <ravenne.leopold@gmail.com>
2024-09-26 16:54:25 +0200

    feat(ecs/SparseArray.hpp): begin and end operators for iteration

--- ecs/include/SparseArray.hpp
+++ ecs/include/SparseArray.hpp
@@ -12,6 +12,9 @@ namespace ecs {
             using reference_type = value_type &;
             using const_reference_type = value_type const &;
             using container = std::vector<value_type>;
+            using size_type = typename container::size_type;
+            using iterator = typename container::iterator;
+            using const_iterator = typename container::const_iterator;
 
             SparseArray() {}
             SparseArray(SparseArray const &other) : _data(other._data) {}
@@ -29,6 +32,30 @@ namespace ecs {
                     _data = std::move(other._data);
                 }
                 return *this;
+            }
+
+            iterator begin() {
+                return _data.begin();
+            }
+
+            const_iterator begin() const {
+                return _data.begin();
+            }
+
+            const_iterator cbegin() const {
+                return _data.cbegin();
+            }
+
+            iterator end() {
+                return _data.end();
+            }
+
+            const_iterator end() const {
+                return _data.end();
+            }
+
+            const_iterator cend() const {
+                return _data.cend();
             }
 
             reference_type operator[](size_t idx) {
