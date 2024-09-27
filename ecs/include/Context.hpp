1ed0f263ed86dec8689e733003dafa16a7304342
Paul Bardeur <paul.bardeur@epitech.eu>
2024-09-27 13:45:23 +0200

    merge(context): merge context master into context server

--- ecs/include/Context.hpp
+++ ecs/include/Context.hpp
@@ -1,35 +1,10 @@
 /*
 ** EPITECH PROJECT, 2024
-<<<<<<< HEAD
-** rtype
-=======
 ** R-Type
->>>>>>> master
 ** File description:
 ** Context
 */
 
-<<<<<<< HEAD
-#pragma once
-
-#include <SFML/Graphics.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-
-namespace ECS {
-
-class Context {
-  public:
-    Context(sf::RenderWindow &);
-    ~Context();
-
-    sf::RenderWindow &getRenderWindow();
-
-  protected:
-  private:
-    sf::RenderWindow &_window;
-};
-} // namespace ECS
-=======
 #ifndef CONTEXT_HPP_
     #define CONTEXT_HPP_
 
@@ -37,6 +12,7 @@ class Context {
     #include <SFML/Graphics/RenderWindow.hpp>
     #include "EntityManager.hpp"
     #include "Registry.hpp"
+  
 namespace ecs {
 
 	class Context {
@@ -57,7 +33,6 @@ namespace ecs {
                     EntityManager _entitys;
                     Registry _r;
 	};
-} // namespace ecs
+}
 
 #endif /* !CONTEXT_HPP_ */
->>>>>>> master
