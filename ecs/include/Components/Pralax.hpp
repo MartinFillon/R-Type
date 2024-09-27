911aaa45252738b805776224465fac1addc7e88a
codEWard-edward <edward.lalande@epitech.eu>
2024-09-27 14:08:07 +0200

    feat(Component): Paralax component for entity with a paralax

---
+++ ecs/include/Components/Pralax.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Paralax
+*/
+
+#ifndef PARALAX_HPP_
+    #define PARALAX_HPP_
+
+namespace ecs {
+    namespace component {
+        struct Paralax {
+          bool _value;
+        };
+    };
+};
+
+#endif /* !PARALAX_HPP_ */
