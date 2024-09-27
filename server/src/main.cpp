ffe1de10797c9a0e352cbe74c0d51651acee10e5
codEWard-edward <edward.lalande@epitech.eu>
2024-09-27 09:53:45 +0200

    fix(Server): server return 0 because he don't handle a window, or the game

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -5,27 +5,7 @@
 ** main
 */
 
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Window/VideoMode.hpp>
-
-#include "Context.hpp"
-
 int main(void)
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "ecs SFML Example");
-
-    ecs::Context ctx = ecs::Context(window);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-
-        window.display();
-    }
     return 0;
 }
