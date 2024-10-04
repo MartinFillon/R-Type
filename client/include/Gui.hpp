dc7f0e5a159c3313abfaefc999cfeb0ad0ea81b4
Paul BARDEUR <paul.bardeur@epitech.eu>
2024-10-04 11:38:20 +0200

    feat(gui): add a class network on the gui to communicate with the server

--- client/include/Gui.hpp
+++ client/include/Gui.hpp
@@ -8,35 +8,54 @@
 #ifndef GUI_HPP_
 #define GUI_HPP_
 
+#include <memory>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
-#include <memory>
-#include "ComponentFactory.hpp"
+
+#include "Network.hpp"
 #include "IContext.hpp"
 #include "Registry.hpp"
+#include "ComponentFactory.hpp"
+
+#define HOST 1
+#define PORT 2
+#define NB_ARGS 3
+#define SUCCESS 0
+#define ERROR 84
 
 namespace rtype {
+
     class Gui : ecs::IContext {
-      public:
-        Gui();
-        ~Gui();
 
-        int run() override;
+        public:
 
-      protected:
-      private:
-        sf::RenderWindow &getRenderWindow();
+            Gui(const std::string &host, const std::string &port);
 
-        void setupPlayer();
-        void setupBackground();
-        void setupWeapon();
-        void setupBasicEnnemies();
+            void start();
 
-        std::shared_ptr<ecs::Registry> _r;
-        sf::RenderWindow _window;
-        void setupCollisons();
-        ecs::ComponentFactory _factory;
+            int run() override;
+
+            std::shared_ptr<ecs::Registry> &getRegistry() {
+                return _r;
+            };
+
+        private:
+
+            Network _network;
+
+            void setupWeapon();
+            void setupPlayer();
+            void setupCollisons();
+            void setupBackground();
+            void setupBasicEnnemies();
+
+            sf::RenderWindow _window;
+
+            std::shared_ptr<ecs::Registry> _r;
+            ecs::ComponentFactory _factory;
+
     };
-} // namespace rtype
+
+};
 
 #endif /* !GUI_HPP_ */
--- client/src/Gui.cpp
+++ client/src/Gui.cpp
@@ -5,117 +5,113 @@
 ** Gui
 */
 
-#include "Gui.hpp"
 #include <iostream>
 
+#include "Gui.hpp"
 #include "ImageResolver.hpp"
-#include "Systems/BasicRandomEnnemiesSystem.hpp"
-#include "Systems/CollisionsSystem.hpp"
+#include "ZipperIterator.hpp"
 #include "Systems/GunFireSystem.hpp"
 #include "Systems/ParallaxSystem.hpp"
+#include "Systems/CollisionsSystem.hpp"
 #include "Systems/PlayerMouvementSystem.hpp"
-#include "ZipperIterator.hpp"
+#include "Systems/BasicRandomEnnemiesSystem.hpp"
 
-namespace rtype {
-    Gui::Gui() : ecs::IContext(), _window(sf::VideoMode(1920, 1080), GAME_NAME), _r(std::make_shared<ecs::Registry>())
-    {
-        _factory = ecs::ComponentFactory(_r);
-        setupBackground();
-        setupPlayer();
-        setupWeapon();
-        setupBasicEnnemies();
-        setupCollisons();
-    }
+rtype::Gui::Gui(const std::string &host, const std::string &port):
+    ecs::IContext(),
+    _network(host, port),
+    _window(sf::VideoMode(1920, 1080), GAME_NAME),
+    _r(std::make_shared<ecs::Registry>()),
+    _factory(ecs::ComponentFactory(_r))
+{
+    setupBackground();
+    setupPlayer();
+    setupWeapon();
+    setupBasicEnnemies();
+    setupCollisons();
+    _network.setRegistry(_r);
+    _window.close();
+}
 
-    Gui::~Gui() {}
+void rtype::Gui::start()
+{
+    std::thread handleNetwork(&rtype::Network::run, std::ref(_network));
+    //std::thread handleGame(&rtype::Gui::run, this);
 
-    sf::RenderWindow &Gui::getRenderWindow()
-    {
-        return _window;
-    }
+    handleNetwork.join();
+    //handleGame.join();
+}
 
-    void Gui::setupCollisons()
-    {
-        ecs::systems::CollisionsSystem collisions;
-        _r->add_system(collisions);
-    }
+void rtype::Gui::setupWeapon()
+{
+    _r->add_system(ecs::systems::GunFireSystem());
+}
 
-    void Gui::setupWeapon()
-    {
-        ecs::systems::GunFireSystem gunSystem;
-        _r->add_system(gunSystem);
-    }
+void rtype::Gui::setupPlayer()
+{
+    _factory.createEntity("config/player.json");
+    _r->add_system(ecs::systems::PlayerMouvementSystem());
+}
 
-    void Gui::setupBasicEnnemies()
-    {
-        ecs::systems::BasicRandomEnnemiesSystem basicEnnemies;
-        _r->add_system(basicEnnemies);
-    }
+void rtype::Gui::setupCollisons()
+{
+    _r->add_system(ecs::systems::CollisionsSystem());
+}
 
-    void Gui::setupPlayer()
-    {
-        _factory.createEntity("config/player.json");
+void rtype::Gui::setupBackground()
+{
+    _factory.createEntity("config/background/background.json");
+    _factory.createEntity("config/background/background_2.json");
+    _factory.createEntity("config/background/background_3.json");
+    _factory.createEntity("config/background/background_4.json");
+    _r->add_system(ecs::systems::ParalaxSystem());
+}
 
-        std::cout << "Player created" << std::endl;
+void rtype::Gui::setupBasicEnnemies()
+{
+    _r->add_system(ecs::systems::BasicRandomEnnemiesSystem());
+}
 
-        ecs::systems::PlayerMouvementSystem playerMovementSystem;
-        _r->add_system(playerMovementSystem);
-    }
+int rtype::Gui::run()
+{
+    auto &drawables = _r->get_components<ecs::component::Drawable>();
+    auto &sprites = _r->get_components<ecs::component::Sprite>();
+    auto &positions = _r->register_component<ecs::component::Position>();
+    auto &animations = _r->register_component<ecs::component::Animations>();
+    auto &size = _r->register_component<ecs::component::Size>();
 
-    void Gui::setupBackground()
-    {
-        _factory.createEntity("config/background/background.json");
-        _factory.createEntity("config/background/background_2.json");
-        _factory.createEntity("config/background/background_3.json");
-        _factory.createEntity("config/background/background_4.json");
-
-        ecs::systems::ParalaxSystem parallaxSystem;
-        _r->add_system(parallaxSystem);
-    }
-
-    int Gui::run()
-    {
-        auto &drawables = _r->get_components<ecs::component::Drawable>();
-        auto &sprites = _r->get_components<ecs::component::Sprite>();
-        auto &positions = _r->register_component<ecs::component::Position>();
-        auto &animations = _r->register_component<ecs::component::Animations>();
-        auto &size = _r->register_component<ecs::component::Size>();
-
-        while (_window.isOpen()) {
-            sf::Event event;
-            while (_window.pollEvent(event)) {
-                if (event.type == sf::Event::Closed) {
-                    _window.close();
-                }
+    while (_window.isOpen()) {
+        sf::Event event;
+        while (_window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                _window.close();
             }
-            _window.clear();
-            _r->run_systems();
-
-            for (auto &&[draws, anim, spri, si, pos] :
-                 ecs::custom_zip(drawables, animations, sprites, size, positions)) {
-                if (!draws || !anim || !spri || !si || !pos) {
-                    continue;
-                }
-                sf::Texture texture;
-                ecs::ImageResolver image(PATH_TO_ASSETS);
-                std::string pathToImage = image.getImage(spri->_pathToSprite);
-                if (pathToImage.empty()) {
-                    continue;
-                }
-                texture.loadFromMemory(
-                    pathToImage.c_str(),
-                    pathToImage.size(),
-                    sf::IntRect(anim->_x, anim->_y, anim->_width, anim->_height)
-                );
-                sf::Sprite sprite;
-                sprite.setPosition(pos->_x, pos->_y);
-                sprite.setScale(si->_width, si->_height);
-                sprite.setTexture(texture);
-                _window.draw(sprite);
-            }
-            _window.display();
         }
-        return EXIT_SUCCESS;
-    }
+        _window.clear();
+        _r->run_systems();
 
-} // namespace rtype
+        for (auto &&[draws, anim, spri, si, pos] :
+                ecs::custom_zip(drawables, animations, sprites, size, positions)) {
+            if (!draws || !anim || !spri || !si || !pos) {
+                continue;
+            }
+            sf::Texture texture;
+            ecs::ImageResolver image(PATH_TO_ASSETS);
+            std::string pathToImage = image.getImage(spri->_pathToSprite);
+            if (pathToImage.empty()) {
+                continue;
+            }
+            texture.loadFromMemory(
+                pathToImage.c_str(),
+                pathToImage.size(),
+                sf::IntRect(anim->_x, anim->_y, anim->_width, anim->_height)
+            );
+            sf::Sprite sprite;
+            sprite.setPosition(pos->_x, pos->_y);
+            sprite.setScale(si->_width, si->_height);
+            sprite.setTexture(texture);
+            _window.draw(sprite);
+        }
+        _window.display();
+    }
+    return SUCCESS;
+}
