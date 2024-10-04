371840d75a63506e03a542b2d017af4d5fb0b320
Paul BARDEUR <paul.bardeur@epitech.eu>
2024-10-04 11:39:23 +0200

    feat(network): add a class packet to serialized and deserialized packet from/to server

---
+++ client/src/Packet.cpp
@@ -0,0 +1,89 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-PAR-5-1-rtype-edward.lalande
+** File description:
+** Packet
+*/
+
+#include "Packet.hpp"
+
+#include "protocol.hpp"
+
+rtype::Packet::Packet(const Message &message) : _isValid(false)
+{
+    if (message.size() < MESSAGE_MIN_SIZE) {
+        return;
+    }
+
+    _magicNumber = GET_MAGIC_NUMBER(message);
+
+    if (_magicNumber != protocol::MAGIC) {
+        return;
+    }
+
+    _opcode = message[MESSAGE_OPCODE];
+
+    if (!isValidOpcode(_opcode)) {
+        return;
+    }
+
+    _arguments.assign(message.begin() + MESSAGE_OPCODE + 1, message.end());
+
+    _isValid = true;
+}
+
+rtype::Packet::Packet(uint8_t opcode, Arguments arguments) : _isValid(false)
+{
+    _magicNumber = protocol::MAGIC;
+
+    if (!isValidOpcode(opcode)) {
+        return;
+    }
+
+    _opcode = opcode;
+    _arguments = arguments;
+
+    _isValid = true;
+}
+
+bool rtype::Packet::isValid() const
+{
+    return _isValid;
+}
+
+uint8_t rtype::Packet::getOpcode() const
+{
+    return _opcode;
+}
+
+rtype::Packet::Arguments rtype::Packet::getArguments() const
+{
+    return _arguments;
+}
+
+rtype::Packet::Message rtype::Packet::toMessage() const
+{
+    Message message;
+
+    message.push_back(static_cast<uint8_t>((_magicNumber >> 24) & 0xFF));
+    message.push_back(static_cast<uint8_t>((_magicNumber >> 16) & 0xFF));
+    message.push_back(static_cast<uint8_t>((_magicNumber >> 8) & 0xFF));
+    message.push_back(static_cast<uint8_t>(_magicNumber & 0xFF));
+
+    message.push_back(_opcode);
+
+    message.insert(message.end(), _arguments.begin(), _arguments.end());
+
+    return message;
+}
+
+bool rtype::Packet::isValidOpcode(uint8_t opcode)
+{
+    for (size_t i = protocol::OBJECT_POSITION; i != protocol::GAME_SPEED; i++) {
+        if (i == opcode) {
+            return true;
+        }
+    }
+
+    return false;
+}
