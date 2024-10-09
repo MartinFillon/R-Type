39a7e2407d6a38f95d00d176a98c7d9adccd41b3
Timothée Plisson <timothee.plisson@epitech.eu>
2024-10-09 20:32:48 +0200

    refacto(ECS): add files for our Parallax system for dodging bad pratices, and add visibility for the rest of our refacto (better for network CO visibility)

---
+++ ecs/src/systems/ParallaxSystem.cpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** r type proj
+** File description:
+** ParallaxSystem
+*/
+
+#include "Systems/ParallaxSystem.hpp"
+
+void ecs::systems::ParalaxSystem::operator()(Registry &r)
+{
+    if (_clock.getMiliSeconds() < BACKGROUND_TICK) {
+        return;
+    }
+    _clock.restart();
+    auto &paralax = r.get_components<ecs::component::Parallax>();
+    auto &positions = r.get_components<ecs::component::Position>();
+    auto &animation = r.get_components<ecs::component::Animations>();
+
+    for (auto &&[pos, para, anim] : ecs::custom_zip(positions, paralax, animation)) {
+        if (anim->_object == ecs::component::Object::Background && anim->_clock.getSeconds() > (int)(1 / 60)) {
+            if (pos->_x <= -SCREEN_WIDTH) {
+                pos->_x = SCREEN_WIDTH * para->_multiplicator;
+            }
+            pos->_x -= para->_speed;
+        }
+    }
+}
