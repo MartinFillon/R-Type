582a08a40564cfc845ac956ce7f782dce47c2081
Ravriely <ravenne.leopold@gmail.com>
2024-10-09 20:56:57 +0200

    refactor(client): using namespace keyword in cpp files

--- client/src/Gui.cpp
+++ client/src/Gui.cpp
@@ -8,33 +8,28 @@
 #include "Gui.hpp"
 #include <SFML/Graphics/Rect.hpp>
 
-namespace rtype {
+rtype::Gui::Gui()
+    : ecs::IContext(), _network(), _window(sf::VideoMode(1920, 1080), GAME_NAME), _menu(_window), _game(_window)
+{
+    _menu.setupMenu();
+}
 
-    Gui::Gui()
-        : ecs::IContext(), _network(), _window(sf::VideoMode(1920, 1080), GAME_NAME),
-         _menu(_window), _game(_window)
-    {
-        _menu.setupMenu();
+int rtype::Gui::setupNetwork(const std::string server_ip, const std::string server_port)
+{
+    if (_network.setup(server_ip, server_port)) {
+        return EXIT_FAILURE;
     }
 
-    int Gui::setupNetwork(const std::string server_ip, const std::string server_port)
-    {
-        if (_network.setup(server_ip, server_port)) {
-            return EXIT_FAILURE;
-        }
+    return EXIT_SUCCESS;
+}
 
-        return EXIT_SUCCESS;
-    }
+int rtype::Gui::run()
+{
+    _game.run();
+    return EXIT_SUCCESS;
+}
 
-    int Gui::run()
-    {
-        _game.run();
-        return EXIT_SUCCESS;
-    }
-
-    int Gui::runNetwork()
-    {
-        return _network.run();
-    }
-
-} // namespace rtype
+int rtype::Gui::runNetwork()
+{
+    return _network.run();
+}
