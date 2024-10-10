8d11d1e8da6ef408f579856840e7dd44999d16a2
Paul BARDEUR <paul.bardeur@epitech.eu>
2024-10-10 16:43:46 +0200

    fix(client): coding-style menu file and lunch menu on gui

--- client/include/Menu.hpp
+++ client/include/Menu.hpp
@@ -6,74 +6,80 @@
 */
 
 #ifndef MENU_HPP_
-#define MENU_HPP_
+    #define MENU_HPP_
 
-#define TITLE_FONT "assets/fonts/ARCADE_I.ttf"
-#define TEXT_FONT "assets/fonts/OpenSans-Semibold.ttf"
-#define BUTTON_PLAY "Play"
-#define BUTTON_IP "Enter IP Adress !"
-#define BUTTON_QUIT "Quit"
-#define BUTTON_OPTIONS "Options"
-#define MENU_TITLE "R-Type by MR.PET"
-#define LEFT_MARGE 800
-#define PLAY_POS_X 400
-#define IP_POS_X 460
-#define OPTIONS_POS_X 550
-#define IP_RECT_POS_X 495
-#define QUIT_POS_X 625
-#define TITLE_POS_X 200
-#define TITLE_POS_Y 450
-#define TEXT_POS_X 500
-#define FONT_SIZE_TITLE 70
-#define FONT_SIZE_IP 17
-#define FONT_SIZE_INPUT 15
-#define RECT_SIZE_X 30
-#define RECT_SIZE_Y 300
-#define OFFSET "offset"
-#define PARA_SPEED 10.f
-#define BG_PATH "assets/sprites/Background/background_2.png"
+    #include <SFML/Graphics.hpp>
+    #include <SFML/Graphics/RenderWindow.hpp>
 
-#include <SFML/Graphics.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
+    #define TITLE_FONT "assets/fonts/ARCADE_I.ttf"
+    #define TEXT_FONT "assets/fonts/OpenSans-Semibold.ttf"
+    #define BUTTON_PLAY "Play"
+    #define BUTTON_IP "Enter IP Adress !"
+    #define BUTTON_QUIT "Quit"
+    #define BUTTON_OPTIONS "Options"
+    #define MENU_TITLE "R-Type by MR.PET"
+    #define LEFT_MARGE 800
+    #define PLAY_POS_X 400
+    #define IP_POS_X 460
+    #define OPTIONS_POS_X 550
+    #define IP_RECT_POS_X 495
+    #define QUIT_POS_X 625
+    #define TITLE_POS_X 200
+    #define TITLE_POS_Y 450
+    #define TEXT_POS_X 500
+    #define FONT_SIZE_TITLE 70
+    #define FONT_SIZE_IP 17
+    #define FONT_SIZE_INPUT 15
+    #define RECT_SIZE_X 30
+    #define RECT_SIZE_Y 300
+    #define OFFSET "offset"
+    #define PARA_SPEED 10.f
+    #define BG_PATH "assets/sprites/Background/background_2.png"
 
-namespace rtype {
+namespace client {
+
     class Menu {
-      public:
-        Menu(sf::RenderWindow &window);
-        ~Menu() = default;
-        void setupMenu();
-        std::string launchMenu();
 
-      private:
-        // text part
-        sf::RenderWindow &_win;
+        public:
 
-        sf::Font _fontTitle;
-        sf::Font _fontText;
-        sf::Text _menutitle[5];
+            Menu(sf::RenderWindow &window);
 
-        // Log part
+            void setupMenu();
+            std::string launchMenu();
 
-        sf::RectangleShape _ipRect;
-        sf::Text _menuDisplayInput;
-        std::string _menuClientInput;
-        char _inputChar;
+        private:
 
-        // menu or ?
+            sf::RenderWindow &_win;
 
-        bool _isWritting;
-        bool _isLogin;
-        bool _isQuitPress;
-        bool _isMenuOpen;
+            sf::Font _fontTitle;
+            sf::Font _fontText;
+            sf::Text _menutitle[5];
 
-        // background
+            // Log part
 
-        sf::Clock _menuClock;
-        sf::Texture _backgroundTexture;
-        sf::Sprite _backgroundSprite;
-        float _bgScaleX;
-        float _bgScaleY;
-        float _bgOffset;
+            sf::RectangleShape _ipRect;
+            sf::Text _menuDisplayInput;
+            std::string _menuClientInput;
+            char _inputChar;
+
+            // menu or ?
+
+            bool _isWritting;
+            bool _isLogin;
+            bool _isQuitPress;
+            bool _isMenuOpen;
+
+            // background
+
+            sf::Clock _menuClock;
+            sf::Texture _backgroundTexture;
+            sf::Sprite _backgroundSprite;
+            float _bgScaleX;
+            float _bgScaleY;
+            float _bgOffset;
+
     };
-} // namespace rtype
+
+};
+
 #endif /* !MENU_HPP_ */
