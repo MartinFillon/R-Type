48b227bc582389fbc09e88e289629ae67d313b01
codEWard-edward <edward.lalande@epitech.eu>
2024-10-10 14:41:27 +0200

    feat(IContext): IContext doxygen documentation

--- ecs/include/IContext.hpp
+++ ecs/include/IContext.hpp
@@ -11,10 +11,14 @@
 #define GAME_NAME "R-Type"
 
 namespace ecs {
+    /// @brief IContext class is an Interface of context. The GUI must have a class for window handling... that heritate of the IContext
     class IContext {
       public:
+        /// @brief virtual default destructor
         virtual ~IContext() = default;
 
+        /// @brief run method to run the GUI
+        /// @return it return an int, 0 in success, and error is what you prefered
         virtual int run() = 0;
     };
 } // namespace ecs
