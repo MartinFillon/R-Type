a247c10f6719f4a22fef4fb7a5cfc4685ea23361
codEWard-edward <edward.lalande@epitech.eu>
2024-10-10 15:23:22 +0200

    feat(PositionComponents): Position Component doxygen Documentation

--- ecs/include/Components/Position.hpp
+++ ecs/include/Components/Position.hpp
@@ -10,6 +10,7 @@
 
 namespace ecs {
     namespace component {
+        /// @brief Position Component, to store the position of the entity, it store as an integer the x and y, and a boolean to know if it change of direction, useful for the milespates or entities that changes directions alone
         struct Position {
             int _x;
             int _y;
