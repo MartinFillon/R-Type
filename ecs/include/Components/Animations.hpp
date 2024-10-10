a6ead3f83122f704c9b68843f64d8e828715f02f
codEWard-edward <edward.lalande@epitech.eu>
2024-10-10 15:07:56 +0200

    feat(AnimationsComponents): Animations Component doxygen Documentation

--- ecs/include/Components/Animations.hpp
+++ ecs/include/Components/Animations.hpp
@@ -11,11 +11,15 @@
 #include "Clock.hpp"
 
 namespace ecs {
+    /// @brief Component namespaces for components type
     namespace component {
+        /// @brief Object is to know what object is in the sparse array and make difference in all Entity
         enum Object { Background, Player, Weapon, Ennemies, InDestroy };
 
+        /// @brief Type is to know wich type this object is, because it can be an Ennemies but wich one ? Basic ? Milespates ? Boss. The Type type is to know to the type of the object
         enum Type { None, Basic, Milespates, Boss };
 
+        /// @brief Animations component to handling the rectangle to set in the image of the texture, it get his clock, width, height, x, y, and a rotation if nedded
         struct Animations {
             ecs::Clock _clock;
             double _width;
