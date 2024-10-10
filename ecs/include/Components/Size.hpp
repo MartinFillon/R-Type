d85a088e795026564b368d0b7d22f4448b699af9
codEWard-edward <edward.lalande@epitech.eu>
2024-10-10 15:27:47 +0200

    feat(SizeComponents): Size Component doxygen Documentation

--- ecs/include/Components/Size.hpp
+++ ecs/include/Components/Size.hpp
@@ -10,6 +10,7 @@
 
 namespace ecs {
     namespace component {
+        /// @brief Size component, store the double in width and height as a double for the size, 1 is the sprite size to upgrade it put it superior to 1, to decrease it, inferior to 1
         struct Size {
             double _width;
             double _height;
