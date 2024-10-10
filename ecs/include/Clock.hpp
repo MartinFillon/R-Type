dd1073087a6b9423cf263691fcace8bb18f36f70
codEWard-edward <edward.lalande@epitech.eu>
2024-10-10 14:57:32 +0200

    feat(Clock): Clock doxygen Documentation

--- ecs/include/Clock.hpp
+++ ecs/include/Clock.hpp
@@ -15,29 +15,38 @@
 
 namespace ecs {
 
+    /// @brief Clock encapsulation to get a Clock and not an sf::Clock
     class Clock {
       public:
+        /// @brief Constructor of Clock, start a clock in his constructor
         Clock()
         {
             _start = std::chrono::system_clock::now();
         }
 
+        /// @brief restart method, reset the clock
         void restart()
         {
             _start = std::chrono::system_clock::now();
         }
 
+        /// @brief getSeconds time elapsed since the last start
+        /// @return the time as a double of the time elapsed since the last start, return it as seconds
         double getSeconds()
         {
             return std::chrono::duration<double>(std::chrono::system_clock::now() - _start).count();
         }
 
+        /// @brief getMiliSeconds time elapsed since the last start
+        /// @return the time as a double of the time elapsed since the last start, return it as Miliseconds
         double getMiliSeconds()
         {
             return std::chrono::duration<double>(std::chrono::system_clock::now() - _start).count() *
                 SECONDS_TO_MILISECONDS;
         }
 
+        /// @brief getMiliSeconds time elapsed since the last start
+        /// @return the time as a double of the time elapsed since the last start, return it as MicroSeconds
         double getMicroSeconds()
         {
             return std::chrono::duration<double>(std::chrono::system_clock::now() - _start).count() *
@@ -45,6 +54,7 @@ namespace ecs {
         }
 
       private:
+        /// @brief is the chrono of the class
         std::chrono::time_point<std::chrono::system_clock> _start;
     };
 
