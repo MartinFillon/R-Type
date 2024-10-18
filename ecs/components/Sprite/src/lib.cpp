7f106b0d7a32b75c7fe378614e2e83c099832f4a
MartinFillon <martin.fillon@hotmail.com>
2024-10-18 16:32:27 +0200

    refactor: add sprite component

---
+++ ecs/components/Sprite/src/lib.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** rtype
+** File description:
+** Position
+*/
+
+#include <nlohmann/json.hpp>
+
+#include "Components/Sprite.hpp"
+#include "Entity.hpp"
+#include "Registry.hpp"
+
+extern void register_component(ecs::Registry &registry, ecs::Entity &entity, const nlohmann::json &component)
+{
+    auto &components = registry.register_component<ecs::component::Sprite>();
+
+    components[entity.getId()] = {component};
+}
