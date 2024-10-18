5a7e49f881f201108fbd39baffb4d0ec5bed54e4
MartinFillon <martin.fillon@hotmail.com>
2024-10-18 16:57:37 +0200

    refactor: move destroyable component

---
+++ ecs/components/Destroyable/src/lib.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** rtype
+** File description:
+** destroyables
+*/
+
+#include <nlohmann/json.hpp>
+
+#include "Components/Destroyable.hpp"
+#include "Entity.hpp"
+#include "Registry.hpp"
+
+extern void register_component(ecs::Registry &registry, ecs::Entity &entity, const nlohmann::json &component)
+{
+    auto &destroyables = registry.register_component<ecs::component::Destroyable>();
+
+    destroyables[entity.getId()] = {component};
+}
