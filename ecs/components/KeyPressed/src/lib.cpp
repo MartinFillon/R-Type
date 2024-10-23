13f308034d340645e31a8d3c78342079cb9e1eeb
codEWard-edward <edward.lalande@epitech.eu>
2024-10-23 14:09:57 +0200

    feat(Component): component with key pressed to store the last key pressed

---
+++ ecs/components/KeyPressed/src/lib.cpp
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2024
+** rtype
+** File description:
+** animations
+*/
+
+#include <nlohmann/json.hpp>
+
+#include "Components/KeyPressed.hpp"
+#include "Entity.hpp"
+#include "Registry.hpp"
+
+extern "C" void register_component(
+    std::shared_ptr<ecs::Registry> &registry,
+    ecs::Entity &entity,
+    const nlohmann::json &component
+)
+{
+    auto &components = registry->register_component<ecs::component::KeyPressed>();
+    ecs::component::Key value = ecs::component::Key::NoneKey;
+
+    if (component == "up") {
+        value = ecs::component::Key::Up;
+    }
+
+    if (component == "down") {
+        value = ecs::component::Key::Down;
+    }
+
+    if (component == "right") {
+        value = ecs::component::Key::Right;
+    }
+
+    if (component == "left") {
+        value = ecs::component::Key::Left;
+    }
+
+    if (component == "punch") {
+        value = ecs::component::Key::Punch;
+    }
+
+    if (component == "kick") {
+        value = ecs::component::Key::Kick;
+    }
+
+
+    components[entity.getId()] = {value};
+}
