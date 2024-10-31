d140c2e01940f08674f1b75192fb587f40d01dce
codEWard-edward <edward.lalande@epitech.eu>
2024-10-31 11:36:57 +0100

    feat(config): config file for plateform in Poc3D

--- poc_raylib/src/Main.cpp
+++ poc_raylib/src/Main.cpp
@@ -1,25 +1,10 @@
-/*******************************************************************************************
-*
-*   raylib [models] example - Load 3d model with animations and play them
-*
-*   Example originally created with raylib 2.5, last time updated with raylib 3.5
-*
-*   Example contributed by Culacant (@culacant) and reviewed by Ramon Santamaria (@raysan5)
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2019-2024 Culacant (@culacant) and Ramon Santamaria (@raysan5)
-*
-********************************************************************************************
-*
-*   NOTE: To export a model from blender, make sure it is not posed, the vertices need to be 
-*         in the same position as they would be in edit mode and the scale of your models is 
-*         set to 0. Scaling can be done from the export menu.
-*
-********************************************************************************************/
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Main
+*/
 
-// #include "raylib.h"
 #include <filesystem>
 #include <iostream>
 #include <spdlog/spdlog.h>
@@ -33,6 +18,7 @@ int main(void)
 
     try {
         poc::Poc game(60);
+        SetTraceLogLevel(LOG_ERROR); 
         return game.run();
     } catch (const std::filesystem::filesystem_error &e) {
         std::cerr << e.what() << std::endl;
