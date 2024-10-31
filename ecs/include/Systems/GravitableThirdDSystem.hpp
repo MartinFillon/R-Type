8b5d01fb14eb6b524eeeda463de8ae963a470c73
codEWard-edward <edward.lalande@epitech.eu>
2024-10-31 10:40:22 +0100

    fix(Systems): Gravitable systems handle where we jump to let us jump on several plateform

--- ecs/include/Systems/GravitableThirdDSystem.hpp
+++ ecs/include/Systems/GravitableThirdDSystem.hpp
@@ -8,7 +8,7 @@
 #ifndef GRAVITABLETHIRDDSYSTEM_HPP_
 #define GRAVITABLETHIRDDSYSTEM_HPP_
 
-#define MAX_Y 5
+#define MAX_Y 8
 #define INITIAL_Y 0
 
 #include "ISystems.hpp"
@@ -17,9 +17,11 @@
 namespace ecs {
     namespace systems {
         class GravitableThirdDSystem : public ISystems {
-          public:
-            void operator()(std::shared_ptr<Registry> &r, std::shared_ptr<IContext> ctx, ComponentFactory &factory)
-                override;
+            public:
+                void operator()(std::shared_ptr<Registry> &r, std::shared_ptr<IContext> ctx, ComponentFactory &factory)
+                    override;
+            private:
+                int findInitialY(std::shared_ptr<Registry> &r);
         };
     }; // namespace systems
 }; // namespace ecs
