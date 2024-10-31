557c6774d5f9bcaa94f1cd91c7124533a2f6c647
codEWard-edward <edward.lalande@epitech.eu>
2024-10-31 10:40:22 +0100

    fix(Systems): Gravitable systems handle where we jump to let us jump on several plateform

--- ecs/src/systems/GravitableThirdDSystem.cpp
+++ ecs/src/systems/GravitableThirdDSystem.cpp
@@ -1,8 +1,57 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** GravitableThirdDSystem
+*/
+
+#include <iostream>
 #include "Systems/GravitableThirdDSystem.hpp"
+#include "Components/Attributes.hpp"
 #include "Components/Gravitable.hpp"
 #include "Components/KeyPressed.hpp"
 #include "Components/Position3D.hpp"
+#include "Components/RectangleShape.hpp"
 #include "ZipperIterator.hpp"
+
+int ecs::systems::GravitableThirdDSystem::findInitialY(std::shared_ptr<Registry> &r)
+{
+    auto &rectangles = r->get_components<ecs::component::RectangleShape>();
+    auto &positions = r->get_components<ecs::component::Position3D>();
+    auto &pos = r->get_components<ecs::component::Position3D>();
+    auto &attributes = r->get_components<ecs::component::Attributes>();
+    int initialY = INITIAL_Y;
+    bool onPlatform = false;
+    size_t j = 0;
+
+    for (; j < pos.size(); ++j) {
+        if (!pos[j] || !attributes[j]) {
+            continue;
+        }
+        if (attributes[j]->_entity_type == ecs::component::Attributes::EntityType::Player) {
+            continue;
+        }
+    }
+
+    for (size_t i = 0; i < positions.size(); ++i) {
+        if (!rectangles[i] || !attributes[i]) {
+            continue;
+        }
+        float rectMinX = positions[i]->_x - rectangles[i]->_length / 2.0f;
+        float rectMaxX = positions[i]->_x + rectangles[i]->_length / 2.0f;
+        float rectMinZ = positions[i]->_z - rectangles[i]->_width / 2.0f;
+        float rectMaxZ = positions[i]->_z + rectangles[i]->_width / 2.0f;
+        if ((pos[j]->_x < rectMaxX && pos[j]->_x > rectMinX) &&
+            (pos[j]->_z < rectMaxZ && pos[j]->_z > rectMinZ)) {
+                onPlatform = true;
+        }
+        if (onPlatform) {
+            initialY = positions[i]->_y + MAX_Y;
+        }
+    }
+
+    return initialY;
+}
 
 void ecs::systems::GravitableThirdDSystem::operator()(
     std::shared_ptr<Registry> &r,
@@ -13,13 +62,16 @@ void ecs::systems::GravitableThirdDSystem::operator()(
     auto &positions = r->get_components<ecs::component::Position3D>();
     auto &gravitables = r->get_components<ecs::component::Gravitable>();
     auto &keys = r->get_components<ecs::component::KeyPressed>();
-    int initialY = INITIAL_Y;
-    int targetY = MAX_Y;
+    auto &attributes = r->get_components<ecs::component::Attributes>();
 
-    for (auto &&[pos, gravit, key] : custom_zip(positions, gravitables, keys)) {
+    int initialY = findInitialY(r);
+    int targetY = MAX_Y + initialY;
 
-        if (key->_value == ecs::component::Key::Jump && !gravit->_isJumping && !gravit->_isFalling &&
-            pos->_y == initialY) {
+    bool onPlatform = false;
+
+    for (auto &&[pos, gravit, key, attr] : custom_zip(positions, gravitables, keys, attributes)) {
+
+        if (key->_value == ecs::component::Key::Jump && !gravit->_isJumping && !gravit->_isFalling) {
             gravit->_isJumping = true;
         }
 
@@ -27,24 +79,40 @@ void ecs::systems::GravitableThirdDSystem::operator()(
             pos->_y += gravit->_gravityFall;
         }
 
-        if (gravit->_isJumping && pos->_y >= targetY) {
+        if ((gravit->_isJumping && pos->_y >= targetY) || (pos->_y > initialY && !gravit->_isJumping && !gravit->_isFalling)) {
             gravit->_isJumping = false;
             gravit->_isFalling = true;
         }
 
         if (gravit->_isFalling) {
-            pos->_y -= gravit->_gravityFall;
 
-            bool onPlatform = false;
+            if (pos->_y > initialY) {
+                pos->_y -= gravit->_gravityFall;
+            }
+
+            if (attr->_entity_type != ecs::component::Attributes::EntityType::Player) {
+                continue;
+            }
+
+            auto &rectangles = r->get_components<ecs::component::RectangleShape>();
 
             for (size_t i = 0; i < positions.size(); ++i) {
-                if (pos->_y <= positions[i]->_y + 1.0 && pos->_y >= positions[i]->_y) {
-                    pos->_y = positions[i]->_y + 1.0;
-                    gravit->_isFalling = false;
-                    gravit->_isJumping = false;
-                    key->_value = ecs::component::Key::NoneKey;
-                    onPlatform = true;
-                    break;
+                if (!rectangles[i] || !attributes[i]) {
+                    continue;
+                }
+
+                float rectMinX = positions[i]->_x - rectangles[i]->_length / 2.0f;
+                float rectMaxX = positions[i]->_x + rectangles[i]->_length / 2.0f;
+                float rectMinZ = positions[i]->_z - rectangles[i]->_width / 2.0f;
+                float rectMaxZ = positions[i]->_z + rectangles[i]->_width / 2.0f;
+
+                if ((pos->_x < rectMaxX && pos->_x > rectMinX) &&
+                    (pos->_z < rectMaxZ && pos->_z > rectMinZ)) {
+                        onPlatform = true;
+                }
+
+                if (onPlatform) {
+                    initialY = positions[i]->_y + 3;
                 }
             }
 
@@ -55,6 +123,7 @@ void ecs::systems::GravitableThirdDSystem::operator()(
 
         if (gravit->_isFalling && pos->_y <= initialY) {
             pos->_y = initialY;
+            gravit->_isJumping = false;
             gravit->_isFalling = false;
             key->_value = ecs::component::Key::NoneKey;
         }
